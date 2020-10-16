8a75aa08cd9fc2540f040e1bb01227e37e00cb35
Jason DiLoreto <jasondiloreto@yahoo.com>
2020-10-16 00:11:09 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+
+function accessElementInArray(array, Index) {
+  return array[Index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift();
+  return chocolateBars;
+}
